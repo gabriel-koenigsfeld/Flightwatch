47dd5fb704f2f059bbc35940428ff64fef25397e
sin-chen <Sina.Oezugurlu@haw-hamburg.de>
2021-06-20 16:14:53 +0200

    Flight Class Variablen
    - added Plane + Schedule to Constructor

--- app/src/main/java/com/example/flightwatch/Flight.java
+++ app/src/main/java/com/example/flightwatch/Flight.java
@@ -2,47 +2,21 @@ package com.example.flightwatch;
 
 public class Flight {
 
-    private String flightName;
-    private String departureTime;
-    private String destinationTime;
-    private String departureCity;
-    private String destinationCity;
-
+    Plane plane;
+    Schedule schedule;
     private int distance;
     private String flightStatus;
 
     //private enum Status
     //{SCHEDULED, DELAYED, DEPARTED, CANCELLED, ARRIVED};
 
-    public Flight(String flightName, String departureTime, String destinationTime, String departureCity, String destinationCity, int distance, String status){
-        this.flightName = flightName;
-        this.departureTime = departureTime;
-        this.destinationTime = destinationTime;
-        this.departureCity = departureCity;
-        this.destinationCity = destinationCity;
+    public Flight(Plane plane, Schedule schedule, int distance, String status){
+        this.plane = plane;
+        this.schedule = schedule;
         this.distance = distance;
         this.flightStatus = status;
     }
 
-    public String getFlightName() {
-        return flightName;
-    }
-
-    public String getDepartureTime() {
-        return departureTime;
-    }
-
-    public String getDestinationTime() {
-        return destinationTime;
-    }
-
-    public String getDepartureCity() {
-        return departureCity;
-    }
-
-    public String getDestinationCity() {
-        return destinationCity;
-    }
 
     public int getDistance(){
         return distance;
