205536e912771bf24b1707d23d4b168e2bcbec17
sin-chen <Sina.Oezugurlu@haw-hamburg.de>
2021-06-21 20:54:03 +0200

    Create Test Methods
    - Flight Tests
    - Plane Tests
    - Schedule Tests

--- app/src/test/java/com/example/flightwatch/FlightTest.java
+++ app/src/test/java/com/example/flightwatch/FlightTest.java
@@ -4,4 +4,14 @@ import junit.framework.TestCase;
 
 public class FlightTest extends TestCase {
 
+    Flight flight = new Flight(new Plane(), new Schedule(), 100, "DELAYED");
+
+    public void testGetDistance(){
+        assertEquals(100, flight.getDistance());
+    }
+
+    public void testGetStatus(){
+        assertEquals("DELAYED", flight.getFlightStatus());
+    }
+
 }
