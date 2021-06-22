90a78ae4849864f84bfe73f571470efb896bdc64
sin-chen <Sina.Oezugurlu@haw-hamburg.de>
2021-06-22 09:45:44 +0200

    Comments Weather Class

--- app/src/main/java/com/example/flightwatch/Weather.java
+++ app/src/main/java/com/example/flightwatch/Weather.java
@@ -4,19 +4,27 @@ import java.util.Random;
 
 public class Weather {
 
+    //Weather Condition
     private String condition;
+
+    //Types of Condition
     private String[] conditions = new String[]{"Sonnig", "Regen", "Stürmisch"};
+
+    //Temperature as Integer
     private int temperature;
 
+    //Weather Constructor
     public Weather(String condition, int temperature){
         this.condition = condition;
         this.temperature = temperature;
     }
 
+    //Random Weather Constructor
     public Weather(){
         this.generateWeather();
     }
 
+    //Getter and Setter
     public String getCondition() {
         return condition;
     }
@@ -35,8 +43,8 @@ public class Weather {
 
     //Method to generate a random Weather object
     public void generateWeather(){
-        int randCondition = new Random().nextInt(conditions.length);
+        int randCondition = new Random().nextInt(conditions.length);                //Pick Condition from Conditions Array
         this.setCondition(conditions[randCondition]);
-        this.setTemperature(new Random().nextInt(35));
+        this.setTemperature(new Random().nextInt(35));                       //Pick Random Integer in Range 25 for Temperature
     }
 }
